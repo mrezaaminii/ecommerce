a54e8dc20d4d86f349af30a23f5acf262ec4fd05
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2023-09-03 21:26:46 +0330

    make index view of the menu

--- resources/views/admin/content/menu/index.blade.php
+++ resources/views/admin/content/menu/index.blade.php
@@ -9,7 +9,7 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item font-size-12"><a href="#">خانه </a></li>
-            <li class="breadcrumb-item font-size-12"><a href="#"> بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#"> بخش محتوا</a></li>
             <li class="breadcrumb-item font-size-12 active" aria-current="page"> منو</li>
         </ol>
     </nav>
@@ -34,24 +34,97 @@
                                 <th>نام منو</th>
                                 <th>منوی والد</th>
                                 <th>لینک منو</th>
-                                <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                                <th>وضعیت</th>
+                                <th class="max-width-16-rem width-11-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
                             </tr>
                         </thead>
                         <tbody>
+                        @foreach($menus as $key => $menu)
                             <tr>
-                                <th>1</th>
-                                <td>خانه</td>
-                                <td>خانه</td>
-                                <td>http://localhost:8000/category/کالای-الکترونیکی</td>
-                                <td class="text-left width-16-rem">
+                                <th>{{$key += 1}}</th>
+                                <td>{{$menu->name}}</td>
+                                <td>{{$menu->parent_id ? $menu->parent->name : 'منوی اصلی'}}</td>
+                                <td>{{$menu->url}}</td>
+                                <td>
+                                    <label for="{{$menu->id}}">
+                                        <input id="{{$menu->id}}" onchange="changeStatus({{$menu->id}})" data-url="{{route('admin.content.menu.status',$menu->id)}}" type="checkbox" @if($menu->status === 1) checked
+
+                                            @endif>
+                                    </label>
+                                </td>
+                                <td class="text-left width-16-rem width-11-rem">
                                     <a href="" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i>حذف </button>
+                                    <form class="d-inline" action="{{route('admin.content.menu.destroy',$menu->id)}}" method="POST" id="deleteForm">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger btn-sm delete"><i class="fa fa-trash-alt"></i> حذف</button>
+                                    </form>
                                 </td>
                             </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </section>
             </section>
         </section>
     </section>
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        function changeStatus(id){
+            var element = $('#' + id);
+            var url = element.attr('data-url');
+            var elementValue = !element.prop('checked');
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function (response) {
+                    if (response.status){
+                        if (response.checked){
+                            element.prop('checked',true);
+                            successToast('منو با موفقیت فعال شد');
+                        }
+                        else{
+                            element.prop('checked',false);
+                            successToast('منو با موفقیت غیر فعال شد');
+                        }
+                    }
+                    else{
+                        element.prop('checked',elementValue);
+                        errorToast('هنگام ویرایش مشکلی پیش آمده است');
+                    }
+                },
+                error: function () {
+                    element.prop('checked',elementValue);
+                    errorToast('ارتباط برقرار نشد');
+                }
+            });
+            function successToast(message){
+                var successToastTag = '<div class="toast" data-delay="5000">\n' +
+                    '<div class="toast-body py-3 d-flex bg-success text-white">\n' +
+                    '<strong class="ml-auto">\n' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' + '</button>\n' + '</div>\n' + '</div>';
+
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function () {
+                    $(this).remove();
+                })
+            }
+            function errorToast(message){
+                var errorToastTag = '<div class="toast" data-delay="5000">\n' +
+                    '<div class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                    '<strong class="ml-auto">\n' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' + '</button>\n' + '</div>\n' + '</div>';
+
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5500).queue(function () {
+                    $(this).remove();
+                })
+            }
+        }
+    </script>
+    @include('admin.alerts.sweetalert.delete-confirm',['className' => 'delete'])
 @endsection
