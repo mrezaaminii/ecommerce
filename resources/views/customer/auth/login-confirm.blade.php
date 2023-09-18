d2e47ce6c9d93228ca7f82fec738a765d46d9611
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2023-09-18 15:22:47 +0330

    making the resend otp code section

--- resources/views/customer/auth/login-confirm.blade.php
+++ resources/views/customer/auth/login-confirm.blade.php
@@ -46,7 +46,7 @@
                 </section>
                 <section class="login-btn d-grid g-2"><button class="btn btn-danger">تایید</button></section>
                 <section id="resend-otp" class="d-none">
-                    <a href="#" class="text-decoration-none text-primary">دریافت مجدد کد تایید</a>
+                    <a href="{{route('auth.customer.login-resend-otp',$token)}}" class="text-decoration-none text-primary">دریافت مجدد کد تایید</a>
                 </section>
                 <section id="timer">
 
