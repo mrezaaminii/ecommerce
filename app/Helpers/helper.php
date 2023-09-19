14ef1509ebca1fe8d56003b0e4550866c86c9699
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2023-09-19 22:15:43 +0330

    writing some useful helpers

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -3,7 +3,42 @@ namespace App\Helpers;
 use Morilog\Jalali\Jalalian;
 
 class helper{
-public static function jalaliDate($date,$format = '%A, %d %B %Y'){
-    return Jalalian::forge($date)->format($format);
-}
+    public static function jalaliDate($date,$format = '%A, %d %B %Y'){
+        return Jalalian::forge($date)->format($format);
+    }
+
+    public function convertPersianToEnglish($number){
+        $number = str_replace('۰','0',$number);
+        $number = str_replace('۱','1',$number);
+        $number = str_replace('۲','2',$number);
+        $number = str_replace('۳','3',$number);
+        $number = str_replace('۴','4',$number);
+        $number = str_replace('۵','5',$number);
+        $number = str_replace('۶','6',$number);
+        $number = str_replace('۷','7',$number);
+        $number = str_replace('۸','8',$number);
+        $number = str_replace('۹','9',$number);
+        return $number;
+    }
+
+    public function convertEnglishToPersian($number){
+        $number = str_replace('0','۰',$number);
+        $number = str_replace('1','۱',$number);
+        $number = str_replace('2','۲',$number);
+        $number = str_replace('3','۳',$number);
+        $number = str_replace('4','۴',$number);
+        $number = str_replace('5','۵',$number);
+        $number = str_replace('6','۶',$number);
+        $number = str_replace('7','۷',$number);
+        $number = str_replace('8','۸',$number);
+        $number = str_replace('9','۹',$number);
+        return $number;
+    }
+
+    public function priceFormat($price){
+        $price = number_format($price,0,'/','،');
+        $price = $this->convertEnglishToPersian($price);
+        return $price;
+    }
+
 }
