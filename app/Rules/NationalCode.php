6f3eeed5cc76be9ddf264283336e73b51c91c671
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2023-10-29 20:47:14 +0330

    making a validation for national code

---
+++ app/Rules/NationalCode.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Rules;
+
+use App\Helpers\helper;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class NationalCode implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail):void
+    {
+        helper::validateNationalCode($value);
+    }
+}
