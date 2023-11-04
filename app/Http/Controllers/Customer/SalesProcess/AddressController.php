f6544c1c32cf0db4cf00f39a9ab3998780528e7f
Mohammad Reza <mohammadreza.amini8294@gmail.com>
2023-11-04 21:00:32 +0330

    adding common discount to discount section and calculations

--- app/Http/Controllers/Customer/SalesProcess/AddressController.php
+++ app/Http/Controllers/Customer/SalesProcess/AddressController.php
@@ -8,11 +8,13 @@ use App\Http\Requests\Customer\SalesProcess\ChooseAddressAndDeliveryRequest;
 use App\Http\Requests\Customer\SalesProcess\StoreAddressRequest;
 use App\Http\Requests\Customer\SalesProcess\UpdateAddressRequest;
 use App\Models\Admin\Market\Address;
+use App\Models\Admin\Market\CommonDiscount;
 use App\Models\Admin\Market\Delivery;
 use App\Models\Admin\Market\Order;
 use App\Models\City;
 use App\Models\Market\CartItem;
 use App\Models\Province;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
@@ -24,12 +26,12 @@ class AddressController extends Controller
     {
         $user = Auth::user();
         $provinces = Province::all();
-        $cartItems = CartItem::where('user_id',$user->id)->get();
-        $deliveryMethods = Delivery::query()->where('status',1)->get();
-        if (empty(CartItem::query()->where('user_id',$user->id)->count())){
+        $cartItems = CartItem::where('user_id', $user->id)->get();
+        $deliveryMethods = Delivery::query()->where('status', 1)->get();
+        if (empty(CartItem::query()->where('user_id', $user->id)->count())) {
             return redirect()->route('customer.sales-process.cart');
         }
-        return view('customer.sales-process.address-and-delivery',compact('cartItems','user','provinces','deliveryMethods'));
+        return view('customer.sales-process.address-and-delivery', compact('cartItems', 'user', 'provinces', 'deliveryMethods'));
     }
 
     public function addAddress(StoreAddressRequest $request): RedirectResponse
@@ -44,15 +46,14 @@ class AddressController extends Controller
     public function getCities(Province $province): JsonResponse
     {
         $cities = $province->cities()->get();
-        if ($cities != null){
-            return response()->json(['status' => true,'cities' => $cities]);
-        }
-        else{
-            return response()->json(['status' => false,'cities' => null]);
+        if ($cities != null) {
+            return response()->json(['status' => true, 'cities' => $cities]);
+        } else {
+            return response()->json(['status' => false, 'cities' => null]);
         }
     }
 
-    public function updateAddress(Address $address,UpdateAddressRequest $request): RedirectResponse
+    public function updateAddress(Address $address, UpdateAddressRequest $request): RedirectResponse
     {
         $inputs = $request->all();
         $inputs['user_id'] = Auth::id();
@@ -64,21 +65,39 @@ class AddressController extends Controller
     {
         $user = Auth::user();
         $inputs = $request->all();
-        $cartItems = CartItem::where('user_id',$user->id)->get();
+        $cartItems = CartItem::where('user_id', $user->id)->get();
         $totalProductPrice = 0;
         $totalDiscount = 0;
         $totalFinalPrice = 0;
         $totalFinalDiscountWithNumbers = 0;
-        foreach ($cartItems as $cartItem){
+        foreach ($cartItems as $cartItem) {
             $totalProductPrice += $cartItem->cartItemProductPrice();
             $totalDiscount += $cartItem->cartItemProductDiscount();
             $totalFinalPrice += $cartItem->cartItemFinalPrice();
             $totalFinalDiscountWithNumbers += $cartItem->cartItemFinalDiscount();
         }
-        $inputs['order_final_amount'] = $totalFinalPrice;
+
+        $commonDiscount = CommonDiscount::query()->where([['status', 1], ['start_date','<',now()], ['end_date','>',now()]])->first();
+        if ($commonDiscount) {
+            $commonPercentageDiscountAmount = $totalFinalPrice * ($commonDiscount->percentage / 100);
+            if ($commonPercentageDiscountAmount > $commonDiscount->discount_ceiling) {
+                $commonPercentageDiscountAmount = $commonDiscount->discount_ceiling;
+            }
+            if ($totalFinalPrice >= $commonDiscount->minimal_order_amount) {
+                $finalPrice = $totalFinalPrice - $commonPercentageDiscountAmount;
+            } else {
+                $finalPrice = $totalFinalPrice;
+            }
+            $inputs['order_final_amount'] = $finalPrice;
+
+        } else {
+            $commonPercentageDiscountAmount = null;
+        }
         $inputs['order_discount_amount'] = $totalFinalDiscountWithNumbers;
+        $inputs['order_common_discount_amount'] = $commonPercentageDiscountAmount;
+        $inputs['order_total_products_discount_amount'] = $inputs['order_common_discount_amount'] + $inputs['order_discount_amount'];
         $inputs['user_id'] = $user->id;
-        $order = Order::query()->updateOrCreate(['user_id' => $user->id,'order_status' => 0],$inputs);
+        $order = Order::query()->updateOrCreate(['user_id' => $user->id, 'order_status' => 0], $inputs);
         return redirect()->route('customer.sales-process.payment');
     }
 }
